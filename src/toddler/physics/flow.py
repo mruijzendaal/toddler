cea97b1e91957d85db2947a35fed0e6ea99941cf
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2025-01-10 22:02:36 +0100

    Improve flow functions

--- src/toddler/physics/flow.py
+++ src/toddler/physics/flow.py
@@ -5,12 +5,24 @@ from toddler.physics.thermodynamics import pressure_to_molar_density
 
 def slm_to_moles(slm):
     """Converts a mass flow rate in Standard Liters per Minute (SLM) to a molar flow rate in mol/s."""
-    return slm * (1e-3 / 60) * atm / R / 273
+    scmm = slm * 1e-3  # Standard Cubic Meters per Minute
+    scms = scmm / 60  # Standard Cubic Meters per Second
+    return scms * (atm / R / 273)
+
+
+def moles_to_slm(moles):
+    """Converts a molar flow rate in mol/s to a mass flow rate in Standard Liters per Minute (SLM)."""
+    return moles * R * 273 / atm * 60
 
 
 def slm_to_massflux(slm, amu):
     """Converts a mass flow rate in Standard Liters per Minute (SLM) to a mass flux in kg/s"""
     return slm_to_moles(slm) * amu / 1000
+
+
+def massflux_to_slm(massflux, amu):
+    """Converts a mass flux in kg/s to a mass flow rate in Standard Liters per Minute (SLM)"""
+    return moles_to_slm(massflux / amu * 1000)
 
 
 def plug_flow_average_mass_flux(slm, r):
